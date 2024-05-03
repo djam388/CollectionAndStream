eeca07e9cbfee105641f948bcec68532b22feaf2
djam388 <djam388@hotmail.com>
2024-05-03 16:50:41 +0500

    commit #3 the error in HW3_1 is solved. All tests are passed.

--- src/main/java/Homework3_1.java
+++ src/main/java/Homework3_1.java
@@ -1,6 +1,8 @@
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Класс для реализации домашнего задания номер три, часть 1.
@@ -14,30 +16,44 @@ public class Homework3_1 {
      */
     public Map<String,Integer> countWords(String text){
 
-        String cleanedTextFromPunctuation = text.replaceAll("[^а-яА-Яa-zA-Z\\s]", " ");
-
-        String trimmedText = cleanedTextFromPunctuation.trim();
-
-
-        String[] words = trimmedText.split("\\s+");
-
-
         Map<String, Integer> wordFrequencies = new HashMap<>();
+        String regex = "[\\{А-Яа-яA-Za-z\\}]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
 
-
-        for (String word : words) {
-            if (word.equals("а")) {
-                System.out.println("found letter 'а'");
-            }
-            wordFrequencies.put(word.toLowerCase(), wordFrequencies.getOrDefault(word, 0) + 1);
+        while (matcher.find()) {
+//            if (matcher.group(0).toLowerCase().equals("а")) {
+//                System.out.println("Full match: " + matcher.group(0));
+//            }
+//            if (wordFrequencies.containsKey(matcher.group(0).toLowerCase())){
+//                int mapVal = wordFrequencies.get(matcher.group(0).toLowerCase());
+//                mapVal++;
+//                wordFrequencies.put(matcher.group(0).toLowerCase(), mapVal);
+//            } else {
+//                wordFrequencies.put(matcher.group(0).toLowerCase(), 1);
+//            }
+            wordFrequencies.put(matcher.group(0).toLowerCase(), wordFrequencies.getOrDefault(matcher.group(0).toLowerCase(), 0) + 1);
         }
+
+
+
+
+
+
+
+//        for (String word : words) {
+//            if (word.equals("а")) {
+//                System.out.println("found letter 'а'");
+//            }
+//            wordFrequencies.put(word.toLowerCase(), wordFrequencies.getOrDefault(word, 0) + 1);
+//        }
 
 
         System.out.println("Word frequencies:");
         for (Map.Entry<String, Integer> entry : wordFrequencies.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-        //return Map.of();
+
         return wordFrequencies;
 
     }
